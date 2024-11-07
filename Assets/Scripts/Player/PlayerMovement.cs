f7ce2c2b2f536b5d50094c81519102b0a0daa85e
TaraDaReal SteamLT <112470727+TaraDaReal@users.noreply.github.com>
2024-11-07 17:46:32 +0200

    11/07
    Add basic movement

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float _moveSpeed = 5f;
+
+    private Vector2 _movement;
+
+    private Rigidbody2D _rigidbody;
+
+    private void Awake()
+    {
+
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        _movement.Set(InputManager.movement.x, InputManager.movement.y);
+
+        _rigidbody.velocity = _movement * _moveSpeed;
+    }
+}
